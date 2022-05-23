036c46b5b3895c46810710db729ec213710d231e
leandronsp <leandronsp@gmail.com>
2022-05-23 02:14:43 -0300

    add test scenario

--- test/chespirito/request_test.rb
+++ test/chespirito/request_test.rb
@@ -23,5 +23,25 @@ module Chespirito
       assert_equal 'test@example.com',            request.cookies['email']
       assert_equal 'http://localhost:3000/login', request.headers['Location']
     end
+
+    def test_request_restful
+      env = {
+        'REQUEST_METHOD' => 'GET',
+        'PATH_INFO' => '/users/:id',
+        'QUERY_STRING' => '',
+        'SERVER_PORT' => 3000,
+        'SERVER_NAME' => 'localhost:3000',
+        'CONTENT_LENGTH' => 412,
+        'HTTP_COOKIE' => 'email=test@example.com',
+        'rack.input' => ''
+      }.merge({})
+
+      request = ::Chespirito::Request.build(env)
+
+      assert_equal 'GET',        request.verb
+      assert_equal '/users/:id', request.path
+
+      assert request.params == {}
+    end
   end
 end
